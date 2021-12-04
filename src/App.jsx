9a5bb62da2533da7bdaeb507d7e171b11a92f0e7
Henrique <henriquenobr@gmail.com>
2021-12-04 18:37:30 -0300

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {BiArchive} from 'react-icons/bi'
 
 function App() {
   return (
-    <div className="App">
-     <h1><BiArchive/>My Appointments</h1>
+    <div className="App container nx-auto mt-3 font-thin">
+     <h1 className="text-5xl"><BiArchive className="inline-block text-red-400 aling-top"/>My Appointments</h1>
     </div>
   );
 }
