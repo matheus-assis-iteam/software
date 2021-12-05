443f2f2c6dbe42640e7b05123d306ae682d5782f
Henrique <henriquenobr@gmail.com>
2021-12-05 15:41:18 -0300

    appointment-info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import {BiArchive} from 'react-icons/bi'
+import { BiCalendar } from 'react-icons/bi'
+import AddAppointment from './components/AddAppointment';
+import Search from './components/Seach';
+import appointmentList from './data.json'
+import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
   return (
     <div className="App container nx-auto mt-3 font-thin">
-     <h1 className="text-5xl"><BiArchive className="inline-block text-red-400 aling-top"/>My Appointments</h1>
+      <h1 className="text-5xl mb-3"><BiCalendar className="inline-block text-red-400 aling-top" />My Appointments</h1>
+      <AddAppointment />
+      <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {appointmentList
+          .map(appointment => (
+           <AppointmentInfo key={appointment.id}
+           appointment={appointment} />
+          ))
+        }
+      </ul>
     </div>
   );
 }
